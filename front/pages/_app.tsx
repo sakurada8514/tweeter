82f7c1a6d7952ecbf9fa8821ed70a2374c22c9c1
櫻田亮太 <tns.r.14xx@gmail.com>
2022-06-26 17:08:26 +0900

    first commit

---
+++ front/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider defaultTheme="dark">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
